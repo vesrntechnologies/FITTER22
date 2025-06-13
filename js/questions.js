9f3b22f93b2e91530c5f81e020cbb7347cd82206
vesrntechnologies <vennjoy@gmail.com>
2025-06-13 15:01:39 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,568 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which special file is used in narrow workpieces?",
+    answer: "Pillar file",
+    options: [
+      "Pillar file",
+      "Square file",
+      "Dread naught file",
+      "Swiss pattern file",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the name of the key shown in the figure?",
+    immg: "img2",
+    answer: "Feather key",
+    options: [
+      "Sunk key",
+      "Feather key",
+      "Flat saddle key",
+      "Hollow saddle key",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the purpose of key?",
+    answer: "To transmit torque",
+    options: [
+      "To transmit torque",
+      "For assembly purpose",
+      "To lock the assembly part",
+      "To permit clearance between mating parts",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the taper ratio of Gib head key way?",
+    answer: "1:100",
+    options: [
+      "1:19",
+      "1:20",
+      "1:50",
+      "1:100",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which key is useful for fitting on tapered shafts?",
+    answer: "Woodruff key",
+    options: [
+      "Feather key",
+      "Gib head key",
+      "Woodruff key",
+      "Flat saddle key",
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which file is used for lock repair and filing hard notches in keys?",
+    answer: "Warding file",
+    options: [
+      "Pillar file",
+      "Flexible file",
+      "Warding file",
+      "Swiss pattern file",
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the screw shown in the figure?",
+    immg: "img7",
+    answer: "Drive screw",
+    options: [
+      "Grub screw",
+      "Drive screw",
+      "Self taping screw",
+      "Thread cutting screw",
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the name of the screw driver shown in the figure?",
+    immg: "img8",
+    answer: "Offset screw driver",
+    options: [
+      "Offset screw driver",
+      "Philips screw driver",
+      "Standard screw driver",
+      "Heavy duty screw driver",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the name of the locking device shown in the figure?",
+    immg: "img9",
+    answer: "Spring washer",
+    options: [
+      "Tab washer",
+      "Locking plate",
+      "Lock washer",
+      "Spring washer",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which file is used for finishing sharp corners?",
+    answer: "Barrette file",
+    options: [
+      "Pillar file",
+      "Riffler file",
+      "Barrette file",
+      "Warding file",
+    ]
+  },
+  {
+    numb: 11,
+    question: "What is the name of the file shown in the figure?",
+    immg: "img11",
+    answer: "Tinker’s file",
+    options: [
+      "Tinker’s file",
+      "Crossing file",
+      "Swiss pattern file",
+      "Dread naught file",
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is the name of the file shown in the figure?",
+    immg: "img12",
+    answer: "Warding file",
+    options: [
+      "Pillar file",
+      "Warding file",
+      "Dread naught file",
+      "Swiss pattern file",
+    ]
+  },
+  {
+    numb: 13,
+    question: "What is the name of the nut shown in the figure?",
+    immg: "img13",
+    answer: "Castle nut",
+    options: [
+      "Castle nut",
+      "Slotted nut",
+      "T - Nut",
+      "Cap nut",
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which type of screw is shown in the figure?",
+    immg: "img14",
+    answer: "Hexagon socket head cap screw",
+    options: [
+      "Hexagon socket head cap screw",
+      "Hexagon head screw",
+      "Counter sink head screw",
+      "Raised cheese hand screw",
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is the name of the file shown in the figure?",
+    immg: "img15",
+    answer: "Swiss pattern file",
+    options: [
+      "Pillar file",
+      "Warding file",
+      "Swiss pattern file",
+      "Dread naught file",
+    ]
+  },
+  {
+    numb: 16,
+    question: "What is the name of the gauge shown in the figure?",
+    immg: "img16",
+    answer: "Snap gauge",
+    options: [
+      "Taper gauge",
+      "Snap gauge",
+      "Progressive plug gauge",
+      "Double end plug gauge",
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which file that has sharp and parallel teeth is used on soft and non-metallic materials?",
+    answer: "Dread naught file",
+    options: [
+      "Pillar file",
+      "Tinker file",
+      "Dread naught file",
+      "Warding file",
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which file used for delicate and intricate work?",
+    answer: "Swiss pattern file",
+    options: [
+      "Swiss pattern file",
+      "Pillar file",
+      "Dread naught file",
+      "Warding file",
+    ]
+  },
+  {
+    numb: 19,
+    question: "What is the purpose of dowel pins in assembly?",
+    answer: "Perfect alignment & quicker disassembly",
+    options: [
+      "Perfect alignment & quicker disassembly",
+      "Imperfect alignment",
+      "Slow disassembly",
+      "Easy maintenance",
+    ]
+  },
+  {
+    numb: 20,
+    question: "What is the name of the part of tubular box spanner marked as x shown in the figure?",
+    immg: "img20",
+    answer: "Tommy bar",
+    options: [
+      "Handle",
+      "Body",
+      "Head",
+      "Tommy bar",
+    ]
+  },
+  {
+    numb: 21,
+    question: "What is the name of the part of adjustable spanner marked as x shown in the figure?",
+    immg: "img21",
+    answer: "Knurl",
+    options: [
+      "Movable jaw",
+      "Stationary jaw",
+      "Handle",
+      "Knurl",
+    ]
+  },
+  {
+    numb: 22,
+    question: "What is the use of air ratchet wrench?",
+    answer: "High speed and less torque",
+    options: [
+      "High speed and less torque",
+      "Low speed",
+      "More torque",
+      "High speed and more torque",
+    ]
+  },
+  {
+    numb: 23,
+    question: "What is the use of tab washers?",
+    answer: "Locking the nut at the corner",
+    options: [
+      "Locking the nut at the corner",
+      "Locking the nut at the center",
+      "Locking the nut from the bottom",
+      "Locking the nut from the top",
+    ]
+  },
+  {
+    numb: 24,
+    question: "What is the use of round key in assembly?",
+    answer: "Secure the component with light torque",
+    options: [
+      "Secure the component with light torque",
+      "High torque",
+      "Unidirectional torque",
+      "Slide along the shaft",
+    ]
+  },
+  {
+    numb: 25,
+    question: "What is the use of rotary files?",
+    answer: "Die sinking and mould-making work",
+    options: [
+      "To finish sharp corners",
+      "To sharpening wood-working saws",
+      "Die sinking and mould-making work",
+      "Finish the automobile bodies, after tinkering",
+    ]
+  },
+  {
+    numb: 26,
+    question: "What is the use of rifflers file?",
+    answer: "Silversmith work",
+    options: [
+      "Finishing sharp corners",
+      "Lock repair",
+      "Silversmith work",
+      "Mould - making work",
+    ]
+  },
+  {
+    numb: 27,
+    question: "Which file is used for silversmith work?",
+    answer: "Riffler file",
+    options: [
+      "Min saw file",
+      "Riffler file",
+      "Pillar file",
+      "Warding file",
+    ]
+  },
+  {
+    numb: 28,
+    question: "What is checked using a template?",
+    answer: "Contour profile of a workpiece",
+    options: [
+      "Diameter of shaft",
+      "Pitch of holes",
+      "Hole size",
+      "Contour profile of a workpiece",
+    ]
+  },
+  {
+    numb: 29,
+    question: "What is checked using a fillet gauge?",
+    answer: "External corner radius",
+    options: [
+      "Pitch of the thread",
+      "Internal radius",
+      "External corner radius",
+      "Gap between mating components",
+    ]
+  },
+  {
+    numb: 30,
+    question: "What is used for placing the instruments during working?",
+    answer: "Felt pad",
+    options: [
+      "Wooden blank",
+      "Felt pad",
+      "Steel sheet",
+      "Cloth",
+    ]
+  },
+  {
+    numb: 31,
+    question: "Which is used to clean the slip gauge?",
+    answer: "Carbon tetrachloride",
+    options: [
+      "Wax",
+      "Kerosene",
+      "Soluble oil",
+      "Carbon tetrachloride",
+    ]
+  },
+  {
+    numb: 32,
+    question: "Which is used to clean the lapping plate after charging?",
+    answer: "Kerosene",
+    options: [
+      "Oil",
+      "Kerosene",
+      "Coolant oil",
+      "Petroleum jelly",
+    ]
+  },
+  {
+    numb: 33,
+    question: "Which material protects slip gauge from rusting?",
+    answer: "Petroleum jelly",
+    options: [
+      "Oil",
+      "Wax",
+      "Kerosene",
+      "Petroleum jelly",
+    ]
+  },
+  {
+    numb: 34,
+    question: "Which abrasive is used for lapping soft steel and non-ferrous metals?",
+    answer: "Fused alumina",
+    options: [
+      "Silicon carbide",
+      "Diamond",
+      "Boron carbide",
+      "Fused alumina",
+    ]
+  },
+  {
+    numb: 35,
+    question: "Which instrument is used in measuring the external diameter shown in the figure?",
+    immg: "img35",
+    answer: "Pair of special jaws by using slip gauge",
+    options: [
+      "Vernier caliper",
+      "Outside caliper",
+      "Parallel leg caliper",
+      "Pair of special jaws by using slip gauge",
+    ]
+  },
+  {
+    numb: 36,
+    question: "What is the purpose of slit provided in the lapping tool?",
+    answer: "For expansion",
+    options: [
+      "For clearance",
+      "For expansion",
+      "To retain abrasive",
+      "To adjust the sleeve",
+    ]
+  },
+  {
+    numb: 37,
+    question: "What is the name of the operation shown in the figure?",
+    immg: "img37",
+    answer: "Lapping large diameter",
+    options: [
+      "Internal ring lapping",
+      "External ring lapping",
+      "Lapping internal cylinder",
+      "Lapping large diameter",
+    ]
+  },
+  {
+    numb: 38,
+    question: "Which type of abrasive is used in honing cast iron and non-ferrous materials?",
+    answer: "Silicon carbide",
+    options: [
+      "Diamond",
+      "Boron carbide",
+      "Silicon carbide",
+      "Aluminium oxide",
+    ]
+  },
+  {
+    numb: 39,
+    question: "What is the direction of frictional force against a motional object?",
+    answer: "Opposite to the object",
+    options: [
+      "Inclined to the object",
+      "Opposite to the object",
+      "Parallel to the object",
+      "Perpendicular to the object",
+    ]
+  },
+  {
+    numb: 40,
+    question: "Which symbol is used to denote co-efficient of friction?",
+    answer: "µ (Meu)",
+    options: [
+      "α (Alpha)",
+      "µ (Meu)",
+      "β (Beta)",
+      "γ (Gamma)",
+    ]
+  },
+  {
+    numb: 41,
+    question: "What is the purpose of a lubricant?",
+    answer: "To reduce friction",
+    options: [
+      "To increase the pressure",
+      "To increase friction",
+      "To reduce friction",
+      "To reduce pressure",
+    ]
+  },
+  {
+    numb: 42,
+    question: "Which is used to reduce the friction in machine parts?",
+    answer: "Lubricants",
+    options: [
+      "Kerosene",
+      "Petrol",
+      "Water",
+      "Lubricants",
+    ]
+  },
+  {
+    numb: 43,
+    question: "Where the centre of gravity of a circle lies?",
+    answer: "At its centre",
+    options: [
+      "At its centre",
+      "Any where on its radius",
+      "Any where on its circumference",
+      "Any where on its diameter",
+    ]
+  },
+  {
+    numb: 44,
+    question: "Which one of the following geometrical shapes centre of gravity lies from its base is 1/3 of its height?",
+    answer: "Triangle",
+    options: [
+      "Square",
+      "Rhombus",
+      "Triangle",
+      "Cone",
+    ]
+  },
+  {
+    numb: 45,
+    question: "Lock nut or check-nut is chamfered on____.",
+    answer: "both side",
+    options: [
+      "upper side",
+      "lower side",
+      "both side",
+      "none sides",
+    ]
+  },
+  {
+    numb: 46,
+    question: "Rivets are generally, specified by____.",
+    answer: "nominal diameter",
+    options: [
+      "length of rivet",
+      "diameter of head",
+      "nominal diameter",
+      "all of these",
+    ]
+  },
+  {
+    numb: 47,
+    question: "Which of the following is not a main part of rivet?",
+    answer: "Point",
+    options: [
+      "Head",
+      "Shank",
+      "Point",
+      "Tail",
+    ]
+  },
+  {
+    numb: 48,
+    question: "The thread angle of metric thread is_____.",
+    answer: "60°",
+    options: [
+      "90°",
+      "60°",
+      "30°",
+      "120°",
+    ]
+  },
+  {
+    numb: 49,
+    question: "The rivet head for a general purpose shown in the figure below is _____",
+    immg: "img49",
+    answer: "counter sunk",
+    options: [
+      "snap",
+      "pan",
+      "counter sunk",
+      "flat",
+    ]
+  },
+  {
+    numb: 50,
+    question: "The head for boiler application's shown in the figure given below is _____",
+    immg: "img50",
+    answer: "steeple",
+    options: [
+      "snap",
+      "pan",
+      "conical",
+      "steeple",
+    ]
+  },
+];
